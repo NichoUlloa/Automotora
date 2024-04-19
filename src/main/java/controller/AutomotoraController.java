7cb031f7f09b7bd4727ec21478eea3e97de1051c
Nicho123 <n.ulloa06@ufromail.cl>
2024-04-19 11:19:21 -0400

    actualizacion Clases y metodos para Automotora

---
+++ src/main/java/controller/AutomotoraController.java
@@ -0,0 +1,8 @@
+package controller;
+import modelo.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutomotoraController {
+}
---
+++ src/main/java/modelo/MarcaVehiculo.java
@@ -0,0 +1,21 @@
+package modelo;
+
+public enum MarcaVehiculo {
+    TOYOTA("Toyota"), CHEVROLET("Chevrolet"), NISSAN("Nissan"), MAZDA("Mazda");
+
+    private final String marcaVehiculo;
+
+
+    private MarcaVehiculo(String marcaVehiculo) {
+        this.marcaVehiculo = marcaVehiculo;
+    }
+
+    public String getMarcaVehiculo() {
+        return marcaVehiculo;
+    }
+
+    @Override
+    public String toString() {
+        return marcaVehiculo;
+    }
+}
